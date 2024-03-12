b193a1f5098dcd34756d76c081db970244b6adf0
daniele.silva <dane.albuquerque14@gmail.com>
2024-03-12 13:25:34 -0400

    adicionando e editando arquivos para as atividades de node

--- node1/index.js
+++ node1/index.js
@@ -1,20 +1,24 @@
 const http = require("http");
 const fs = require("fs");
 
+const dotenv = require("dotenv");
+dotenv.config({ path: `.env.${process.env.NODE_ENV}`});
+
+const PORT = process.env.PORT ?? 9999
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/html;charset=utf-8"});
     fs.readdir(process.argv[2], (err, files) => {
         if (err) console.log(err);
         else {
             files.forEach(file => {
-                res.write(file);
+                res.write(`${file} <br>`);
             })
+            res.end();
         }
     })
-    res.end();
 })
 
-server.listen(5555, () => {
-    console.log("Servidor rodando na porta 5555");
-    console.log(__dirname)
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 })
